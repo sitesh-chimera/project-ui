2ed2f8c161160fc4d4da2776e7f8926418107ef9
sitesh-chimera <siteshr@chimeratechnologies.com>
2021-05-05 18:37:41 +0530

    Added e2e testing for add device.

---
+++ cypress/integration/test.device.js
@@ -0,0 +1,16 @@
+describe("Should add device to the system", () => {
+  it("Add Mobile", () => {
+    cy.visit("http://localhost:3000/dashboard");
+    cy.get(".add-device_button").click();
+    cy.get("input#formBasicDeviceName")
+      .type("Samsung Mobile")
+      .should("have.value", "Samsung Mobile");
+    cy.get("input#formBasicOS")
+      .type("Android Operation System")
+      .should("have.value", "Android Operation System");
+    cy.get("input#formBasicManufacturer")
+      .type("Samsung Company")
+      .should("have.value", "Samsung Company");
+    cy.get(".save-device_button").click();
+  });
+});
